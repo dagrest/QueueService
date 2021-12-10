b1ca43a6d670b3611b17efdcd0c351d5eab23c4a
David Agrest <david.agrest@sap.com>
2021-12-10 16:01:40 +0200

    Initial commit

---
+++ QueueService/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace QueueService
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; private set; }
+        public ILifetimeScope AutofacContainer { get; private set; }
+        public Startup(IHostEnvironment env)
+        {
+            //just in case we want to add config later
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();            
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddCarter();
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            AutofacContainer = builder.Build();
+            return new AutofacServiceProvider(AutofacContainer);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(builder => builder.MapCarter());
+        }
+    }
+}
